4d39af27406f2bf9d6d1e256cf5934690779937f
Ivan Tokarev <ivantokarev@MBP-Ivan.Dlink>
2020-05-02 19:13:58 +0300

    Tops added to stats page.

--- src/app/pages/estate-statistics/estate-statistics.component.ts
+++ src/app/pages/estate-statistics/estate-statistics.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {RestService} from '../../services/rest.service';
+import {Subject} from 'rxjs';
+import {finalize, takeUntil} from 'rxjs/operators';
+import {Router} from '@angular/router';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-estate-statistics',
   templateUrl: './estate-statistics.component.html',
   styleUrls: ['./estate-statistics.component.scss']
 })
-export class EstateStatisticsComponent implements OnInit {
+export class EstateStatisticsComponent implements OnInit, OnDestroy {
+  isFetching = true;
+  displayedColumns: string[] = ['area', 'sell_price', 'rent_price', 'coeff'];
+  dataSource: MatTableDataSource<any>;
 
-  constructor() { }
+  private readonly destroy$ = new Subject();
+
+  constructor(private rest: RestService, private router: Router) { }
 
   ngOnInit(): void {
+    this.rest.getEntities('top')
+      .pipe(
+        takeUntil(this.destroy$),
+        finalize(() => this.isFetching = false)
+      )
+      .subscribe(data => {
+        console.log('Top: ', data);
+        this.dataSource = new MatTableDataSource(data);
+      });
   }
 
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  navigate(url: string) {
+    this.router.navigate([url]);
+  }
+
+  onTopEstateClicked(estate: any) {
+    console.log('Estate clicked: ', estate);
+    const sellUrlAarray = estate.sell_url.split('/');
+    const rentUrlArray = estate.rent_url.split('/');
+
+
+    this.router.navigate([]).then(() => { window.open('/estate?sell=' + sellUrlAarray[sellUrlAarray.length - 1] + '&rent=' +
+      rentUrlArray[rentUrlArray.length - 1], '_blank'); });
+  }
 }
--- src/app/pages/estate-statistics/estate-statistics.module.ts
+++ src/app/pages/estate-statistics/estate-statistics.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EstateStatisticsComponent } from './estate-statistics.component';
 import {EstateStatisticsRoutingModule} from './estate-statistics-routing.module';
+import {UtilModule} from '../../util/util.module';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatTableModule} from '@angular/material/table';
+
+const MatModules = [MatProgressSpinnerModule, MatTableModule];
 
 @NgModule({
   declarations: [EstateStatisticsComponent],
   imports: [
     CommonModule,
-    EstateStatisticsRoutingModule
+    EstateStatisticsRoutingModule,
+    UtilModule,
+    MatModules
   ]
 })
 export class EstateStatisticsModule { }
